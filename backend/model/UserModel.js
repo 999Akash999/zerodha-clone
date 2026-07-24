04d0d1d7785eec7a108e941a8c782115b6baad92
Akash <gangwarakash555@gmail.com>
2026-07-24 23:26:42 +0530

    Add phone number field to UserModel

--- backend/model/UserModel.js
+++ backend/model/UserModel.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema(
   {
     name: { type: String, trim: true, required: true },
     email: { type: String, trim: true, lowercase: true, required: true, unique: true },
+    phone: { type: String, trim: true, required: true, unique: true },
     password: { type: String, required: true, minlength: 6 },
   },
   { timestamps: true }
